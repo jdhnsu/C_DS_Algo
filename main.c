2f811acca95b2af229673b8792724f2047e28eeb
Jdhggg <24016020834@stu.nsu.edu.cn>
2025-03-17 14:33:15 +0800

    改错

--- main.c
+++ main.c
@@ -6,8 +6,10 @@
 int main(void)
 {
 
-    sq_list list;
-    init_sq_list(&list);
+    sq_list *list = init_sq_list(&list);
+    list->length = 6;
+    replace_sq_list(list, 2, 5);
+    print_sq_list(list);
     printf("Hello World!\n");
     return 0;
 }
--- sq_list.c
+++ sq_list.c
@@ -7,7 +7,6 @@ sq_list* init_sq_list(sq_list *name)
 {
     name = (sq_list*)malloc(sizeof(sq_list));
     memset(name, 0, sizeof(sq_list));
-    return name;
 }
 
 // 删除
@@ -53,7 +52,7 @@ void insert_sq_list(sq_list* list, int pos, int value)
     }
     if (pos == MAX)
         printf("有损插入\n");
-    if 9pos > list->length && pos <= MAX)
+    if (pos > list->length && pos <= MAX)
         printf("不建议在此处插入");
     int i = list->length;
     int flog = i;
